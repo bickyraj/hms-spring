e9811359e93073e4e96f8158da0be36102c1285b
bicky raj kayastha <bickyrajkayastha@gmail.com>
2026-06-29 11:40:18 +0200

    updates

--- app/src/main/java/com/hms/app/configurations/SecurityConfig.java
+++ app/src/main/java/com/hms/app/configurations/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -15,22 +17,23 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
-				.csrf(Customizer.withDefaults())
-				.oauth2ResourceServer(oauth2 -> {
-					oauth2.jwt(jwtConfigurer -> jwtConfigurer
-							.jwtAuthenticationConverter(jwtAuthenticationConverter()));
-				})
+				.csrf(CsrfConfigurer::disable)
+				.cors(CorsConfigurer::disable)
+//				.oauth2ResourceServer(oauth2 -> {
+//					oauth2.jwt(jwtConfigurer -> jwtConfigurer
+//							.jwtAuthenticationConverter(jwtAuthenticationConverter()));
+//				})
 				.authorizeHttpRequests((authorize) -> {
-					authorize.anyRequest().hasRole("USER");
+					authorize.anyRequest().permitAll();
 				});
 
 		return http.build();
 	}
 
-	@Bean
-	public JwtAuthenticationConverter jwtAuthenticationConverter() {
-		JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
-		converter.setJwtGrantedAuthoritiesConverter(new KeycloakAuthenticationConverter());
-		return converter;
-	}
+//	@Bean
+//	public JwtAuthenticationConverter jwtAuthenticationConverter() {
+//		JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+//		converter.setJwtGrantedAuthoritiesConverter(new KeycloakAuthenticationConverter());
+//		return converter;
+//	}
 }
