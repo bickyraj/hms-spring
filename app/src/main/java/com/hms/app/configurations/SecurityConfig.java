9104ab2dd49ae8d3c63ce217d85ef60936a9d88f
bicky raj kayastha <bickyrajkayastha@gmail.com>
2026-07-02 16:53:39 +0200

    updats

--- app/src/main/java/com/hms/app/configurations/SecurityConfig.java
+++ app/src/main/java/com/hms/app/configurations/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.hms.app.configurations;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -10,6 +12,8 @@ import org.springframework.security.config.annotation.web.configurers.CsrfConfig
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -18,8 +22,8 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
-				.csrf(CsrfConfigurer::disable)
-				.cors(CorsConfigurer::disable)
+				.csrf(Customizer.withDefaults())
+				.cors(Customizer.withDefaults())
 				.oauth2ResourceServer(oauth2 -> {
 					oauth2.jwt(jwtConfigurer -> jwtConfigurer
 							.jwtAuthenticationConverter(jwtAuthenticationConverter()));
